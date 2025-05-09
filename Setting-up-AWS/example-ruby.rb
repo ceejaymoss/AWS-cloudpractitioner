1cca18b048bce6d7e66d357c10ab215249751075
ceejaymoss <130158448+ceejaymoss@users.noreply.github.com>
2025-05-09 10:37:19 +0000

    final-edit

--- Setting-up-AWS/example-ruby.rb
+++ Setting-up-AWS/example-ruby.rb
@@ -1,14 +1,20 @@
 require 'aws-sdk-s3'
 require 'pry'
 
-client = Aws::S3::Client.new(
-  region: 'ap-southeast-2',
-  #credentials: credentials,
-)
+aws.config.update{{
+  region: 'ap-southeast-2'
+}}
+
+client = Aws::S3::Client.new{{
+  credentials: Aws::credentials.new{
+    'your_access_key_id',
+    'your_secret_access_key'
+  }
+}}
 
 resp = s3_list_buckets
 puts resp.inspect
 bundle.pry
 
-https://docs.aws.amazon.com/sdk-for-ruby/v3/developer-guide/welcome.html
-https://docs.aws.amazon.com/sdk-for-ruby/v3/api/Aws/S3/Client.html
+# https://docs.aws.amazon.com/sdk-for-ruby/v3/developer-guide/welcome.html
+# https://docs.aws.amazon.com/sdk-for-ruby/v3/api/Aws/S3/Client.html
